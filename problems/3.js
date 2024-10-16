29779dcf0485e5d269e63624bce5d1fc026f2a20
Joe Rogers <frogers@uci.edu>
2024-10-15 21:41:58 -0700

    Solved problem 3

--- problems/3.js
+++ problems/3.js
@@ -7,7 +7,11 @@
  * @example [1, 2, 3, 4, 5, 6, 7, 8, 9] -> [3, 6, 9]
  */
 function problem(numbers) {
-    return null;
+    let array = numbers.filter(function(item) {
+        return item % 3 === 0;
+    });
+
+    return array;
 }
 
 const tests = [
